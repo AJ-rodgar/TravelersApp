52e8c13cd409daa8337cdc35847ceb3a79b14a01
AJ <ajrodgar@gmail.com>
2020-01-06 18:57:13 +0100

    Primera toma de contacto

---
+++ app/src/main/java/rodriguezgarcia/antoniojesus/travelersapp/MainActivity.java
@@ -0,0 +1,14 @@
+package rodriguezgarcia.antoniojesus.travelersapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
